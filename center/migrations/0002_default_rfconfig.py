916c1b79fd3eed5059f81fa0c440ac012b060e80
Richard Kojedzinszky <krichy@nmdps.net>
2016-06-23 16:22:06 +0200

    creating a default RFConfig entry

---
+++ center/migrations/0002_default_rfconfig.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+def create_rfconfig(apps, schema_editor):
+    import random, os
+    generator = random.SystemRandom()
+
+    apps.get_model('center', 'RFConfig').objects.create(
+        pk=1,
+        rf_channel=generator.randrange(256),
+        rf_profile=apps.get_model('center', 'RFProfile').objects.get(pk=1),
+        network_id=generator.randrange(65536),
+        aes_key=''.join('%02x' % ord(c) for c in os.urandom(16)),
+    )
+
+def remove_rfconfig(apps, schema_editor):
+    apps.get_model('center', 'RFConfig').objects.all().delete()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('center', '0001_initial'),
+    ]
+
+    operations = [
+            migrations.RunPython(create_rfconfig, reverse_code=remove_rfconfig),
+    ]
