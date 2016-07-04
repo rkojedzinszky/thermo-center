371567402b87b7a380694e6f75f7b8980cf00bca
Richard Kojedzinszky <krichy@nmdps.net>
2016-07-04 15:42:07 +0200

    basic tastypie resources, support classes

---
+++ application/resource.py
@@ -0,0 +1,43 @@
+""" resource-related classes, overriding tastypie defaults """
+
+from tastypie.resources import Resource as tResource, ModelResource as tModelResource
+from tastypie.authentication import BasicAuthentication, ApiKeyAuthentication, SessionAuthentication, MultiAuthentication
+
+class Resource(tResource):
+    class Meta:
+        authentication = MultiAuthentication(SessionAuthentication(), ApiKeyAuthentication(), BasicAuthentication())
+        always_return_data = True
+        limit = 0
+        max_limit = 0
+
+class ModelResource(tModelResource):
+    class Meta(Resource.Meta):
+        pass
+
+from tastypie.authorization import Authorization
+
+class NoAuthorization(Authorization):
+    def read_list(self, object_list, bundle):
+        return []
+
+    def read_detail(self, object_list, bundle):
+        return False
+
+    def create_list(self, object_list, bundle):
+        return []
+
+    def create_detail(self, object_list, bundle):
+        return False
+
+    def update_list(self, object_list, bundle):
+        return []
+
+    def update_detail(self, object_list, bundle):
+        return False
+
+    def delete_list(self, object_list, bundle):
+        return []
+
+    def delete_detail(self, object_list, bundle):
+        return False
+
