c0a9923ec88dd8332924646d661a736eac41d812
Richard Kojedzinszky <krichy@nmdps.net>
2016-08-28 20:45:36 +0200

    daemon startup script tuned

--- bin/start-daemons.sh
+++ bin/start-daemons.sh
@@ -6,8 +6,10 @@ cd "$ROOT"
 export LANG=en_US.UTF-8
 export PYTHONHASHSEED=random
 
+~/graphite/bin/carbon-cache.py stop
+rm -f ~/graphite/storage/carbon-cache-a.pid
 ~/graphite/bin/carbon-cache.py start
 
 rm -f receiver.sock
 python manage.py receiver -d
-uwsgi --uwsgi-socket 127.0.0.1:8080 --wsgi-file=application/wsgi.py --master --workers 2 --threads 8 --daemonize=/dev/null
+uwsgi --ini uwsgi.ini
