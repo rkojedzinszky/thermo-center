9349d94ae37e99b1ccbecc0f0be512a44ea3fbd3
Richard Kojedzinszky <krichy@nmdps.net>
2016-09-27 13:39:23 +0200

    heatcontrol.DayTime: checking for overlaps

--- heatcontrol/models.py
+++ heatcontrol/models.py
@@ -1,6 +1,8 @@
 
+import datetime
 from center.models import Sensor
-from django.db import models
+from django.db import models, IntegrityError
+from django.core.exceptions import ValidationError
 
 class DayType(models.Model):
     """ Represents day types """
@@ -25,6 +27,22 @@ class DayTime(models.Model):
 
     def __str__(self):
         return '%s[%s-%s]' % (self.daytype, self.start, self.end)
+
+    def save(self, *args, **kwargs):
+        if self.end != datetime.time(0, 0) and self.end < self.start:
+            raise ValidationError()
+
+        qs = DayTime.objects.filter(daytype=self.daytype).filter(models.Q(end='00:00:00') | models.Q(end__gt=self.start))
+        if self.end != datetime.time(0, 0):
+            qs = qs.filter(start__lt=self.end)
+
+        if self.pk is not None:
+            qs = qs.exclude(pk=self.pk)
+
+        if qs.exists():
+            raise IntegrityError()
+
+        return super(DayTime, self).save(*args, **kwargs)
 
 class HeatSensor(models.Model):
     """ A specific target temperature setting for a sensor for a given daytime """
