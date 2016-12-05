3d04274ca471949bf31af55faa8fb3299cca0ea2
Richard Kojedzinszky <krichy@nmdps.net>
2016-12-05 15:36:50 +0100

    build: wiping dist/

--- www/build.sh
+++ www/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 set -e
-test -d dist && find dist -type f -name '*.gz' -delete
+test -d dist && find dist -mindepth 1 -maxdepth 1 -print0 | xargs -r0 rm -rf
 nodejs build.js
 find dist/ -type f -regex '.*.\(js\|css\)' -print0 | xargs -t -r0 gzip -k9
