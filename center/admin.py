6846c99d60bc3eb8969021091ae5a5b61f71fef2
Richard Kojedzinszky <krichy@nmdps.net>
2017-09-09 07:57:53 +0200

    center.admin updated

--- center/admin.py
+++ center/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from center import models
 
-admin.site.register(models.RFProfile)
-admin.site.register(models.RFConfig)
-admin.site.register(models.Sensor)
+class RFProfileAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(models.RFProfile, RFProfileAdmin)
+
+class RFConfigAdmin(admin.ModelAdmin):
+    list_display = ('rf_channel', 'rf_profile', 'network_id')
+    actions = None
+
+admin.site.register(models.RFConfig, RFConfigAdmin)
+
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'last_seq', 'last_tsf')
+    readonly_fields = ('last_seq', 'last_tsf')
+
+admin.site.register(models.Sensor, SensorAdmin)
