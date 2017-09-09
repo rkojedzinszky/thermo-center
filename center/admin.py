0bd3dbc7090dfa2d353bb9ac15e5b069d52622aa
Richard Kojedzinszky <krichy@nmdps.net>
2017-09-09 08:33:54 +0200

    center.admin: added some orderings

--- center/admin.py
+++ center/admin.py
@@ -3,6 +3,7 @@ from center import models
 
 class RFProfileAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    ordering = ('id',)
 
 admin.site.register(models.RFProfile, RFProfileAdmin)
 
@@ -15,5 +16,6 @@ admin.site.register(models.RFConfig, RFConfigAdmin)
 class SensorAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'last_seq', 'last_tsf')
     readonly_fields = ('last_seq', 'last_tsf')
+    ordering = ('id',)
 
 admin.site.register(models.Sensor, SensorAdmin)
