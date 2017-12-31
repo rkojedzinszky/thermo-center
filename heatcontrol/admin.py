c533a9a65ead6396550c48f09031f37f6385115f
Richard Kojedzinszky <krichy@nmdps.net>
2017-12-31 12:54:16 +0100

    heatcontrol: admin/list for Calendar tuned

--- heatcontrol/admin.py
+++ heatcontrol/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from heatcontrol import models
+from django.utils import timezone
+
+class CalendarAdmin(admin.ModelAdmin):
+    list_display = ('day', 'daytype')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(day__gte=timezone.now()).order_by('day')
 
 admin.site.register(models.DayType)
-admin.site.register(models.Calendar)
+admin.site.register(models.Calendar, CalendarAdmin)
 admin.site.register(models.Control)
 admin.site.register(models.Profile)
 admin.site.register(models.ScheduledOverride)
