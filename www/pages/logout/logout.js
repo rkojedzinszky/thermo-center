b510bdefed165d83ed30fc3ba812c600c9ef5fcc
Richard Kojedzinszky <krichy@nmdps.net>
2018-12-28 18:49:12 +0100

    Logout fixed

--- www/pages/logout/logout.js
+++ www/pages/logout/logout.js
@@ -1,28 +1,11 @@
 'use strict';
-// import {Component} from 'can';
 import queues from 'can-queues';
 
-/*
-Component.extend({
-	tag: 'thermo-p-logout',
-	view: `Logging out...`,
-	ViewModel: {
-		connectedCallback(element) {
-			var self = this;
-			this.app.session.destroy().then(function() {
-				queues.batch.start();
-				self.app.url.page = '';
-				self.app.session = null;
-				queues.batch.stop();
-			});
-		}
-	}
-});
-*/
-
 export default function (app) {
-	queues.batch.start();
-	app.url.page = '';
-	app.session = null;
-	queues.batch.stop();
+	app.session.destroy().then(function() {
+		queues.batch.start();
+		app.url.page = '';
+		app.session = null;
+		queues.batch.stop();
+	});
 };
