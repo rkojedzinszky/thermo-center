7b07bd4983d6b6d56193c030636091d0272156b6
Richard Kojedzinszky <krichy@nmdps.net>
2018-12-29 14:25:39 +0100

    Preparing dockerization

--- www/build.sh
+++ www/build.sh
@@ -2,5 +2,5 @@
 
 set -e
 test -d dist && find dist -mindepth 1 -maxdepth 1 -print0 | xargs -r0 rm -rf
-nodejs build.js
+node build.js
 find dist/ -type f -regex '.*.\(js\|css\)' -print0 | xargs -t -r0 gzip -k9
