061fbd989329600bcc528442dc0d18c7857d50d8
Richard Kojedzinszky <richard@kojedz.in>
2019-06-04 17:49:37 +0200

    receiver component

---
+++ receiver/main.py
@@ -0,0 +1,118 @@
+import sys
+import os
+import spidev
+import logging
+import time
+from . import (
+    radio, config, receiver
+)
+
+logger = logging.getLogger(__name__)
+
+class Daemon:
+    """ Main receiver class """
+    def __init__(self, args):
+        self.args = args
+        self.radio = None
+        self.gpio = None
+        self.task = None
+        self.task_class = receiver.Receiver
+
+    def _init_radio(self):
+        dev = spidev.SpiDev()
+        dev.open(self.args.spi_bus, self.args.spi_cs)
+        dev.mode = self.args.spi_mode
+        dev.max_speed_hz = self.args.spi_freq
+        self.radio = radio.Radio(dev)
+
+    async def _start_receiver(self):
+        """ Set receive mode """
+        await self._set_radio_task(receiver.Receiver)
+
+    async def _start_discoverer(self):
+        """ Set discover mode """
+        pass
+
+    async def _set_radio_task(self, cls):
+        if self.radio_task:
+            await self.radio_task.stop()
+        if cls:
+            self.radio_task = cls(
+                    loop=self.loop,
+                    config=self.config,
+                    radio=self.radio,
+                    gpio=self.gpio,
+                    mqtt=self.mqtt,
+                    )
+            await self.radio_task.start()
+        else:
+            self.radio_task = None
+
+    def daemonize(self):
+        """ Become a daemon """
+        pid = os.fork()
+        if pid > 0:
+            sys.exit(0)
+
+        devnullfd = os.open('/dev/null', os.RDWR)
+        os.dup2(devnullfd, sys.stdin.fileno())
+        os.dup2(devnullfd, sys.stdout.fileno())
+        os.dup2(devnullfd, sys.stderr.fileno())
+        os.close(devnullfd)
+
+    def run(self):
+        """ Main entrypoint """
+
+        os.chdir('/')
+
+        if self.args.daemonize:
+            self.daemonize()
+
+        self._init_radio()
+
+        while True:
+            self.task = self.task_class(args=self.args, radio=self.radio)
+            logger.info('%s: starting', self.task)
+            try:
+                self.task.run()
+            except Exception as e:
+                logger.warn('%s: exception', self.task, exc_info=True)
+            else:
+                logger.info('%s: finished', self.task)
+
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+    import argparse
+    import os
+
+    parser = argparse.ArgumentParser(description='Thermo center receiver daemon')
+    parser.add_argument('--aggregator-host', default=os.environ.get('AGGREGATOR_HOST', 'aggregator'),
+            help='Aggregator hostname/address')
+    parser.add_argument('--aggregator-port', type=int, default=int(os.environ.get('AGGREGATOR_PORT', '8079')),
+            help='Aggregator port')
+    parser.add_argument('--configurator-host', default=os.environ.get('CONFIGURATOR_HOST', 'configurator'),
+            help='Configurator hostname/address')
+    parser.add_argument('--configurator-port', type=int, default=int(os.environ.get('CONFIGURATOR_PORT', '8079')),
+            help='Configurator port')
+    parser.add_argument('--spi-bus', type=int, default=int(os.environ.get('SPI_BUS_NUM', '0')),
+            help='SPI Bus number')
+    parser.add_argument('--spi-cs', type=int, default=int(os.environ.get('SPI_CS_NUM', '0')),
+            help='SPI Chip-select number')
+    parser.add_argument('--spi-mode', type=int, default=int(os.environ.get('SPI_MODE', '0')),
+            help='SPI mode')
+    parser.add_argument('--spi-freq', type=int, default=int(os.environ.get('SPI_FREQ', '100000')),
+            help='SPI frequency')
+    parser.add_argument('--gpio-dir', default=os.environ.get('GPIO_DIR', '/gpio'),
+            help='GPIO dir for interrupt')
+    parser.add_argument('--daemonize', type=bool, default=False,
+            help='Become a daemon')
+
+    logging.basicConfig(stream=sys.stderr, level=logging.WARNING)
+
+    logging.getLogger('receiver').setLevel(logging.INFO)
+
+    config = parser.parse_args()
+
+    Daemon(config).run()
---
+++ receiver/receiver.py
@@ -0,0 +1,152 @@
+
+import asyncio
+import grpc
+import logging
+import struct
+import json
+import time
+from . import (
+        radio, config
+)
+from configurator import api_pb2 as cfg_msg, api_pb2_grpc as cfg_grpc
+from aggregator import api_pb2 as agg_msg, api_pb2_grpc as agg_grpc
+
+WATCHDOG_TIMEOUT = 300
+
+logger = logging.getLogger(__name__)
+
+
+class Receiver(radio.Base):
+    name = 'receiver'
+
+    def _read_config(self):
+        """ Read configuration from configurator """
+        channel = grpc.insecure_channel('{}:{}'.format(self.args.configurator_host, self.args.configurator_port),
+                                        (
+                    ('grpc.keepalive_time_ms', 10000),
+                    ('grpc.keepalive_timeout_ms', 1000),
+                )
+            )
+        stub = cfg_grpc.ConfiguratorStub(channel)
+
+        cfg = config.Config(
+            stub.GetRadioCfg(cfg_msg.RadioCfgRequest(cluster=1)))
+
+        channel.close()
+
+        return cfg
+
+    def _create_astub(self):
+        """ Creates stub to aggregator """
+        channel = grpc.insecure_channel('{}:{}'.format(self.args.aggregator_host, self.args.aggregator_port),
+                                        (
+                    ('grpc.keepalive_time_ms', 10000),
+                    ('grpc.keepalive_timeout_ms', 1000),
+                )
+            )
+
+        return agg_grpc.AggregatorStub(channel)
+
+
+    async def _setup_radio(self):
+        await self.radio.setup_basic()
+        self.radio.xfer2(self.config.radio_config)
+        await self.radio.setup_for_rx()
+        self.radio.wcmd(radio.Radio.CommandStrobe.SRX)
+
+    # This handles one cycle:
+    # 1. resets, configures radio
+    # 2. Enters receiving loop
+    async def arun(self):
+        self.config = await self.loop.run_in_executor(None, self._read_config)
+        self.astub = await self.loop.run_in_executor(None, self._create_astub)
+        self.inflight = 0
+
+        await self._setup_radio()
+        self.gpio.resettbf()
+
+        while True:
+            try:
+                packets = await asyncio.wait_for(self.receive_many(), timeout=WATCHDOG_TIMEOUT)
+            except asyncio.TimeoutError:
+                logger.warn('Watchdog timeout, resetting radio')
+                self._setup_radio()
+            else:
+                for packet in packets:
+                    await self.receive(packet)
+
+    async def receive_many(self):
+        packets = []
+        while len(packets) == 0:
+            await self.gpio.waitforinterrupt()
+
+            data_len = self.radio.status(radio.Radio.StatusReg.RXBYTES)
+            logger.debug('CC1101.RXBYTES=%d' % data_len)
+
+            if data_len & 0x80:
+                logger.warn('CC1101 RX_OVERFLOW')
+                self.radio.wcmd(radio.Radio.CommandStrobe.SFRX)
+                await self.radio.wait_sidle()
+                self.radio.wcmd(radio.Radio.CommandStrobe.SRX)
+                continue
+
+            if data_len > 54:
+                logger.warn('CC1101 suspicious RXBYTES')
+                await self.radio.sidle()
+                self.radio.wcmd(radio.Radio.CommandStrobe.SFRX)
+                self.radio.wcmd(radio.Radio.CommandStrobe.SRX)
+                continue
+
+            while data_len >= 18:
+                # read one packet from radio
+                packets.append(self.radio.read_rxfifo(18))
+                data_len -= 18
+
+        return packets
+
+    async def receive(self, data):
+        start = time.time()
+
+        try:
+            await self._receive_packet(data)
+        except Exception as e:
+            logger.error('error processing packet: %s' % str(e))
+
+        end = time.time()
+
+        logger.debug('Packet processed in %f seconds' % (end - start))
+
+    async def _receive_packet(self, packet):
+        rssi = struct.unpack('b', bytes([packet[16]]))[0] / 2.0 - 74
+        lqi = packet[17] & 0x7f
+        packet = self.config.cipher.decrypt(bytes(packet[:16]))
+
+        network, seq, length, id_ = struct.unpack('<HLBB', packet[:8])
+
+        logger.debug('packet header: network=%04x seq=%08x len=%02x id=%02x' % (
+            network, seq, length, id_))
+
+        if length < 8:
+            logger.error(
+                'Invalid packet data received, short length: %d' % length)
+            return
+        if length > 16:
+            logger.error(
+                'Invalid packet data received, large length: %d' % length)
+            return
+
+        if network != self.config.network:
+            logger.warn('Received packet for invalid network: %d' % network)
+            return
+
+        raw = bytes(packet[8:length])
+
+        sensorpacket = agg_msg.SensorPacket(
+                id=id_,
+                seq=seq,
+                rssi=rssi,
+                lqi=lqi,
+                raw=raw
+                )
+
+        await self.loop.run_in_executor(None, self.astub.FeedSensorPacket, packet)
