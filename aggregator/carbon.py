edb40559a9e59d489f0806265a14e9657c485ada
Richard Kojedzinszky <richard.kojedzinszky@euronetrt.hu>
2020-01-20 13:02:10 +0100

    Linting code

--- aggregator/carbon.py
+++ aggregator/carbon.py
@@ -26,8 +26,8 @@ class LineClient(aiothread.AIOThread):
                     await self.loop.sock_sendall(sock, await self.queue.get())
             except asyncio.CancelledError:
                 break
-            except Exception as e:
-                print (e)
+            except Exception as exc:  # pylint: disable=broad-except
+                print(exc)
                 await asyncio.sleep(1)
             finally:
                 sock.close()
@@ -36,12 +36,14 @@ class LineClient(aiothread.AIOThread):
         self.queue.dput(payload)
 
     def send(self, data):
+        """ Enqueue data to be sent """
+
         if not self.queue:
             return
 
-        payload = ''.join([
-                '{} {} {}\n'.format(r[0], r[1][0], r[1][1])
-                for r in data]
-                )
+        payload = ''.join(
+            ['{} {} {}\n'.format(r[0], r[1][0], r[1][1])
+             for r in data]
+        )
         payload = payload.encode()
         asyncio.run_coroutine_threadsafe(self._send(payload), self.loop)
