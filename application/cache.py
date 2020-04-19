d5aa1ab82f6dea4f5bc32c32f828618f9924c4a4
Richard Kojedzinszky <richard@kojedz.in>
2020-04-20 01:24:45 +0200

    api: simplified cache class, dropped unused method

--- application/cache.py
+++ application/cache.py
@@ -7,35 +7,23 @@ from django.conf import settings
 class _Cache:
     """ Cache class that uses pylibmc pooling to access Memcached """
     def __init__(self):
-        self._pool: pylibmc.ClientPool = None
-
-    def pool(self) -> pylibmc.ClientPool:
-        if self._pool is None:
-            self._pool = pylibmc.ThreadMappedPool(
-                pylibmc.Client(
-                    servers=['{}:{}'.format(settings.MEMCACHED_HOST, settings.MEMCACHED_PORT)],
-                    behaviors={"tcp_keepalive": True, "tcp_nodelay": True}
-                ),
-            )
-
-        return self._pool
+        self.pool = pylibmc.ThreadMappedPool(
+            pylibmc.Client(
+                servers=['{}:{}'.format(settings.MEMCACHED_HOST, settings.MEMCACHED_PORT)],
+                behaviors={"tcp_keepalive": True, "tcp_nodelay": True}
+            ),
+        )
 
     @retry.retry(exceptions=pylibmc.ConnectionError, tries=3, jitter=1, logger=None)
     def set(self, key: str, value: str, time: int=0) -> bool:
         """ Set a key in the cache """
-        with self.pool().reserve() as mc:
+        with self.pool.reserve() as mc:
             return mc.set(key, value, time=time)
-
-    @retry.retry(exceptions=pylibmc.ConnectionError, tries=3, jitter=1, logger=None)
-    def add(self, key: str, value: str, time: int=0) -> bool:
-        """ Add a key in the cache """
-        with self.pool().reserve() as mc:
-            return mc.add(key, value, time=time)
 
     @retry.retry(exceptions=pylibmc.ConnectionError, tries=3, jitter=1, logger=None)
     def get(self, key: str, default: str=None) -> str:
         """ Get a key from the cache """
-        with self.pool().reserve() as mc:
+        with self.pool.reserve() as mc:
             return mc.get(key, default)
 
 cache = _Cache()
