3937778077d3f8493c9c3398e76bd041d7e3e17a
Richard Kojedzinszky <richard@kojedz.in>
2025-06-15 10:41:41 +0200

    feat: add ReadonlyPkeyResource to mark primary keys as readonly

--- application/resource.py
+++ application/resource.py
@@ -1,13 +1,37 @@
-""" resource-related classes, overriding tastypie defaults """
+"""resource-related classes, overriding tastypie defaults"""
 
-from tastypie.authentication import BasicAuthentication, ApiKeyAuthentication, SessionAuthentication, MultiAuthentication
+from tastypie import resources
+from tastypie.authentication import (
+    BasicAuthentication,
+    ApiKeyAuthentication,
+    SessionAuthentication,
+    MultiAuthentication,
+)
 from tastypie.authorization import Authorization
 
+
+class ReadonlyPkeyResource(resources.ModelResource):
+    @classmethod
+    def get_fields(cls, fields=None, excludes=None):
+        final_fields = super().get_fields(fields, excludes)
+
+        # Set readonly on primary_key field
+        for name, field in final_fields.items():
+            djangofield = cls._meta.object_class._meta.get_field(name)
+            if djangofield.primary_key:
+                field.readonly = True
+
+        return final_fields
+
+
 class ResourceMetaCommon:
-    authentication = MultiAuthentication(SessionAuthentication(), ApiKeyAuthentication(), BasicAuthentication())
+    authentication = MultiAuthentication(
+        SessionAuthentication(), ApiKeyAuthentication(), BasicAuthentication()
+    )
     always_return_data = True
     limit = 0
     max_limit = 0
+
 
 class NoAuthorization(Authorization):
     def read_list(self, object_list, bundle):
@@ -33,4 +57,3 @@ class NoAuthorization(Authorization):
 
     def delete_detail(self, object_list, bundle):
         return False
-
