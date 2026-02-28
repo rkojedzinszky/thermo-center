36dc9d4b20d7eb1a8dc05536af2cbbff9a996ec4
Richard Kojedzinszky <richard@kojedz.in>
2026-02-28 14:56:51 +0100

    fix(www): iterate over array in csrf.ts the right way

--- www/src/utils/csrf.ts
+++ www/src/utils/csrf.ts
@@ -3,11 +3,11 @@ export function getCookie(name: string): string | null {
   let cookieValue = null
   if (document.cookie && document.cookie !== '') {
     const cookies = document.cookie.split(';')
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim()
+    for (const cookie of cookies) {
+      const trimmed = cookie.trim()
       // Does this cookie string begin with the name we want?
-      if (cookie.substring(0, name.length + 1) === name + '=') {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
+      if (trimmed.substring(0, name.length + 1) === name + '=') {
+        cookieValue = decodeURIComponent(trimmed.substring(name.length + 1))
         break
       }
     }
